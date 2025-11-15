c816b12afbc96e6adfafaf4af88a6fdf6dc5b247
Danny Stark <1ironman19101@gamil.com>
2025-11-15 23:21:58 +0100

    deployment test 1 again

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from App import create_app, socketio
+import os
 #Run the flask application
 app = create_app()
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    socketio.run(app)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app,  host='0.0.0.0', port=port)
